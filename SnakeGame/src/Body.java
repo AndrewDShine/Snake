516282c79c20964c1d414a90c77039921430e7c9
Andrew Shine <Andrew Shine@DESKTOP-4RK9KPG>
2018-10-20 13:52:03 -0600

    Began making the POJOs of the snake and body -Andrew

---
+++ SnakeGame/src/Body.java
@@ -0,0 +1,8 @@
+
+public class Body 
+	{
+//		This is the Body POJO, aka each piece of the snake.
+	
+		private int xPos; //X and Y positions of the piece.
+		private int yPos;
+	}
---
+++ SnakeGame/src/Snake.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Snake 
+	{
+//		This is the POJO for the whole snake, which will hold an ArrayList of Body objects.
+		private ArrayList<Body> snakeBody = new ArrayList<Body>();
+	}
